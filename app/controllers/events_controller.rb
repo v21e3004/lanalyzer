75afd57c0cf7f5fdc4232b708642e3b35c54c770
v21e3004 <v21e3004@oita-u.ac.jp>
2022-07-21 02:37:58 +0000

    コメント追加

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,16 +1,27 @@
 class EventsController < ApplicationController
   protect_from_forgery
   def index
+    # ログインしている教師のコースのレコードを取得
     @courses = current_user.courses.all
+    # 上記のレコードでfocus: trueのレコードを取得
     @focus_course = current_user.courses.find_by(focus: true)
+    # focus: trueであるコースに属している学生を取得
     @users = @focus_course.users.where(role: "Student")
+    # 上記の学生で課題を提出している学生レコードを取得
     @submitted_student = @users.joins(:events).where.not(events: {submitted_time: nil})
+    # 課題を提出していない学生レコードを取得
     @not_submitted_student = @users.joins(:events).where(events: {submitted_time: nil})
+    # 課題を提出していない学生の人数を代入
     @all_not_submitted_student = @not_submitted_student.count
+    # 課題を提出している学生の人数を代入
     @all_submitted_student = @submitted_student.count
+    # モニタリングする学生すべての人数
     @all_student = @submitted_student.count + @not_submitted_student.count
+    # すべての学生人数を３で割った時の商を取得
+    # 例：４÷３＝1.333....の１を@divnum に代入
     @divnum = @all_student.div(3)
     
+    # 1/3の学生が提出しているかどうかの処理
     if @divnum <= @all_not_submitted_student
       flash[:notice] = "メッセージが送信されました"
       # redirect_to root_path
@@ -20,8 +31,10 @@ class EventsController < ApplicationController
   end
 
   def create
+    # POSTされたCaliper Eventをパース
     caliper_event = JSON.parse(request.body.read)
     
+    # 変数に代入
     # 学生氏名
     stname = caliper_event["data"][0]["actor"]["name"]
     
